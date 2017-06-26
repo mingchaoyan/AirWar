8b1ce308d12d4765c028e32167d9f41aaee71cfa
mingchaoyan <mingchaoyan@gmail.com>
2017-06-26 16:18:19 +0800

    敌机出动

--- src/Game.js
+++ src/Game.js
@@ -7,11 +7,41 @@ var Game = (function() {
     })();
     function onLoaded() {
         this.hero = new Role();
+        this.hero.init("hero", 0, 1, 0, 30);
         this.hero.pos(200, 500);
         Laya.stage.addChild(this.hero);
         Laya.stage.on(Laya.Event.MOUSE_MOVE, this, onmousemove);
+        Laya.timer.frameLoop(1, this, onLoop);
+    }
+    function onLoop() {
+        for(var i = Laya.stage.numChildren - 1; i>0; i--) {
+            var role = Laya.stage.getChildAt(i);
+            if(role && role.speed) {
+                role.y+=role.speed;
+                if(role.y>1000) {
+                    role.removeSelf();
+                    Laya.Pool.recover("role", role);
+                }
+            }
+        }
+        if(Laya.timer.currFrame%60 === 0) {
+            createEnemy(2);
+        }
     }
     function onmousemove() {
         this.hero.pos(Laya.stage.mouseX, Laya.stage.mouseY);
+    }
+    this.hps = [1, 2, 10];
+    this.speed = [3, 2, 1];
+    this.radius = [15, 30, 70];
+    function createEnemy(num) {
+        for(var i=0;i<num;i++) {
+            var r = Math.random();
+            var type = r<0.7?0:r<0.95?1:2;
+            var enemy = Laya.Pool.getItemByClass("role", Role);
+            enemy.init("enemy" + (type+1), 1, this.hps[type], this.speed[type], this.radius[type]);
+            enemy.pos(Math.random()*400+40, Math.random()*100);
+            Laya.stage.addChild(enemy);
+        }
     }
 })();
--- src/Role.js
+++ src/Role.js
@@ -1,20 +1,41 @@
 var Role = (function(_super){
     function Role() {
         Role.super(this);
-        this.init();
+        //this.init();
     }
+    Role.cached = false;
     Laya.class(Role, "Role", _super);
     var _proto = Role.prototype;
-    _proto.init = function() {
-        Laya.Animation.createFrames(["war/hero_fly1.png", "war/hero_fly2.png"], "hero_fly");
-        Laya.Animation.createFrames(["war/hero_down1.png", "war/hero_down2.png", "war/hero_down3.png", "war/hero_down4.png"], "hero_down");
-        this.body = new Laya.Animation();
-        this.addChild(this.body);
-        this.playAction("hero_down");
+    _proto.init = function(_type, _camp, _hp, _speed, _hitRadius) {
+        this.type = _type;
+        this.camp = _camp;
+        this.hp = _hp;
+        this.speed = _speed;
+        this.hitRadius = _hitRadius;
+        if (!Role.cached) {
+            Laya.Animation.createFrames(["war/hero_fly1.png", "war/hero_fly2.png"], "hero_fly");
+            Laya.Animation.createFrames(["war/hero_down1.png", "war/hero_down2.png", "war/hero_down3.png", "war/hero_down4.png"], "hero_down");
+
+            Laya.Animation.createFrames(["war/enemy1_fly1.png"], "enemy1_fly");
+            Laya.Animation.createFrames(["war/enemy1_down1.png", "war/enemy1_down2.png", "war/enemy1_down3.png", "war/enemy1_down4.png"], "enemy1_down");
+
+            Laya.Animation.createFrames(["war/enemy2_fly1.png"], "enemy2_fly");
+            Laya.Animation.createFrames(["war/enemy2_down1.png", "war/enemy2_down2.png", "war/enemy2_down3.png", "war/enemy2_down4.png"], "enemy2_down");
+            Laya.Animation.createFrames(["war/enemy2_hit.png", "enemy2_hit"]);
+            
+            Laya.Animation.createFrames(["war/enemy3_fly1.png", "war/enemy3_fly2.png"], "enemy3_fly");
+            Laya.Animation.createFrames(["war/enemy3_down1.png", "war/enemy3_down2.png", "war/enemy3_down3.png", "war/enemy3_down4.png", "war/enemy3_down5.png", "war/enemy3_down6.png"], "enemy3_down");
+            Laya.Animation.createFrames(["war/enemy3_hit.png", "enemy3_hit"]);
+        }
+        if (!this.body) {
+            this.body = new Laya.Animation();
+            this.addChild(this.body);
+        }
+        this.playAction("fly");
     }
 
     _proto.playAction = function(action) {
-        this.body.play(0, true, action);
+        this.body.play(0, true, this.type + "_"+action);
         this.bound = this.body.getBounds();
         this.body.pos(-this.bound.width/2, -this.bound.height/2);
     }
