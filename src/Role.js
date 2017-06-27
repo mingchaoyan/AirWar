2f3bfb8dfd0bb6436656c4fe038ebcb5bcf981f7
mingchaoyan <mingchaoyan@gmail.com>
2017-06-27 17:09:38 +0800

    ani not found

--- src/Role.js
+++ src/Role.js
@@ -27,11 +27,11 @@ var Role = (function(_super){
 
             Laya.Animation.createFrames(["war/enemy2_fly1.png"], "enemy2_fly");
             Laya.Animation.createFrames(["war/enemy2_down1.png", "war/enemy2_down2.png", "war/enemy2_down3.png", "war/enemy2_down4.png"], "enemy2_down");
-            Laya.Animation.createFrames(["war/enemy2_hit.png", "enemy2_hit"]);
+            Laya.Animation.createFrames(["war/enemy2_hit.png"], "enemy2_hit");
             
             Laya.Animation.createFrames(["war/enemy3_fly1.png", "war/enemy3_fly2.png"], "enemy3_fly");
             Laya.Animation.createFrames(["war/enemy3_down1.png", "war/enemy3_down2.png", "war/enemy3_down3.png", "war/enemy3_down4.png", "war/enemy3_down5.png", "war/enemy3_down6.png"], "enemy3_down");
-            Laya.Animation.createFrames(["war/enemy3_hit.png", "enemy3_hit"]);
+            Laya.Animation.createFrames(["war/enemy3_hit.png"], "enemy3_hit");
 
             Laya.Animation.createFrames(["war/bullet1.png"], "bullet1_fly");
         }
